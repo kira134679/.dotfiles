ba907566e0638e769445d30c1bdfc7f23855f380
kira134679 <kira134679@gmail.com>
2025-09-08 00:06:10 +0800

    feat: add plugins setting

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,30 @@
+local is_transparent = true
+
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = is_transparent,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+    keys = {
+      {
+        "<leader>k",
+        function()
+          is_transparent = not is_transparent
+          local theme = require("tokyonight")
+          theme.setup(
+            {
+              transparent = is_transparent
+            }
+          )
+          vim.cmd("colorscheme tokyonight")
+        end,
+        desc = "Toggle Background"
+      }
+    }
+  },
+}
